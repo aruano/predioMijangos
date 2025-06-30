a32dfff97f827b6f4c74a775966cc2da32476ab8
aruano <aruano@uno-ca.com>
2025-06-29 18:06:38 -0600

    Configuración Inicial

---
+++ backend/src/main/java/com/predio/mijangos/PredioMijangosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.predio.mijangos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredioMijangosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredioMijangosBackendApplication.class, args);
+	}
+
+}
